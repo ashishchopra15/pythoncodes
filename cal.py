0454d29c005bce58fd5ca0cf86760fe86a32cca4
ashish chopra <ashishchopra15@yahoo.com>
2016-10-22 15:27:12 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -4,12 +4,6 @@ c=a+b
 d=a-b
 e=a/b
 f=a*b
-PRINT "DHINKA cHika"
-
-print "ankit ki maa ka"
-print "ankit sala chaprasi"
-print "ankit ghusaa"
-print "yo yo ankita bansal"
 print "sum = " + str(c)
 print "subtract = " + str(d)
 print "multiply = " + str(f)
